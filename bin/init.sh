e727929497c99fd7e452025303505fc4a2956e79
son-of-satan <mtataryn555@gmail.com>
2022-05-13 02:41:23 +0300

    add install/init scripts and enumerate pacman packages

---
+++ bin/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+emacs --batch --eval "
+(progn
+  (require 'org)
+  (setq org-confirm-babel-evaluate nil)
+  (org-babel-tangle-file \"~/.config/doom/config.org\"))
+"
+
+~/.config/doom/bin/install.sh
+
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs
+
+~/.config/emacs/bin/doom -y install
+~/.config/doom/bin/setup.sh
