4dbd868454edf8ed4abf2669835986c2345a4323
unholynuisance <mtataryn555@gmail.com>
2024-05-01 19:56:25 +0300

    remove anything related to packaging

--- bin/init.sh
+++ bin/init.sh
@@ -6,9 +6,6 @@ emacs --batch --eval "
   (org-babel-tangle-file \"~/.config/doom/config.org\"))
 "
 
-# ~/.config/doom/bin/install.sh
-
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.config/emacs
 
 ~/.config/emacs/bin/doom -y install || ~/.config/emacs/bin/doom build
-~/.config/doom/bin/setup.sh
